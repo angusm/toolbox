5cd3c62d3410cb58f470016fe57da472e6c424a6
Angus MacDonald <angusamacdonald@hotmail.com>
2017-12-01 23:14:25 -0500

    Removed hasAttribute, use DOM .hasAttribute

--- utils/dom/has-attribute.js
+++
@@ -1,8 +0,0 @@
-const getAsList = require('../iterable/get-as-list');
-
-function hasAttribute(element, attributeName) {
-  return getAsList(element.attributes)
-    .some((attribute) => attribute.name === attributeName);
-}
-
-module.exports = hasAttribute;
