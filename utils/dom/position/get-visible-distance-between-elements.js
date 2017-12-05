0eff77e797bb1432f8f00a07fca48af09862057c
Angus MacDonald <angusamacdonald@hotmail.com>
2017-12-05 00:14:56 -0500

    Removed search for common ancestor from getVisibleDistanceBetweenElements

--- utils/dom/position/get-visible-distance-between-elements.js
+++ utils/dom/position/get-visible-distance-between-elements.js
@@ -1,10 +1,8 @@
-const getCommonOffsetAncestor = require('./get-common-offset-ancestor');
 const getVisibleDistanceFromAncestor = require('./get-visible-distance-from-ancestor');
 
 function getVisibleDistanceBetweenElements(a, b) {
-    const commonAncestor = getCommonOffsetAncestor(a, b);
-    return getVisibleDistanceFromAncestor(a, commonAncestor)
-        .subtract(getVisibleDistanceFromAncestor(b, commonAncestor));
+    return getVisibleDistanceFromAncestor(a, null)
+        .subtract(getVisibleDistanceFromAncestor(b, null));
 }
 
 module.exports = getVisibleDistanceBetweenElements;
