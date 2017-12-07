2076d53f83c42e7bad8d288f57522bfed326ff0e
Angus MacDonald <angusamacdonald@hotmail.com>
2017-12-06 23:52:30 -0500

    Removed partial refactor of ElementMask

--- utils/dom/element-mask.js
+++ utils/dom/element-mask.js
@@ -3,8 +3,6 @@
  * should otherwise be large amounts of inline content.
  * Created by angusm on 05/12/17.
  */
-const Dimension = require('../cached-vectors/dimension');
-const Dimensions2d = require('../math/geometry/dimensions-2d');
 const Range = require('../range');
 const Scroll = require('../cached-vectors/scroll');
 const Vector2d = require('../math/geometry/vector-2d');
@@ -12,18 +10,12 @@ const getVisibleDimensions = require('./position/get-visible-dimensions');
 const getVisibleDistanceFromAncestor = require('./position/get-visible-distance-from-ancestor');
 const renderLoop = require('../render-loop');
 
-const ZERO_DIMENSIONS = new Dimensions2d();
-const ZERO_VECTOR = new Vector2d();
-
-const windowDimension = Dimension.getSingleton();
 const windowScroll = Scroll.getSingleton();
 
 class ElementMask{
   constructor(fixedElement, maskElement) {
     this.fixedEl_ = fixedElement;
     this.maskEl_ = maskElement;
-    this.maskPosition_ = ZERO_VECTOR;
-    this.maskDimensions_ = ZERO_DIMENSIONS;
     this.stopped_ = false;
     this.init_();
   }
@@ -52,7 +44,7 @@ class ElementMask{
       return;
     }
     renderLoop.measure(() => {
-      this.maskPosition_ = getVisibleDistanceFromAncestor(this.maskEl_);
+      const position = getVisibleDistanceFromAncestor(this.maskEl_);
       if (position.y >= 0) {
         this.renderAbsolute_();
       } else {
