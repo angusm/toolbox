51c506c04c507e95084c0edbea255d2273b1a65f
Angus MacDonald <angusamacdonald@hotmail.com>
2018-01-22 16:26:04 -0500

    Removed special table handling

--- utils/math/geometry/vector-2d.js
+++ utils/math/geometry/vector-2d.js
@@ -1,54 +1,47 @@
 const Matrix = require('../../dom/position/matrix');
 const Vector = require('./vector');
-const isTableDisplayed = require('../style/is-table-displayed');
 
 class Vector2d extends Vector {
-    constructor(x = 0, y = 0) {
-        super(x, y);
-    }
+  constructor(x = 0, y = 0) {
+    super(x, y);
+  }
 
-    get x() {
-        return this.getValues()[0];
-    }
+  get x() {
+    return this.getValues()[0];
+  }
 
-    get y() {
-        return this.getValues()[1];
-    }
+  get y() {
+    return this.getValues()[1];
+  }
 
-    static fromElementOffset(element) {
-        if (element.offsetParent && isTableDisplayed(element.offsetParent)) {
-            return new Vector2d(
-              element.offsetLeft - element.offsetParent.offsetLeft,
-              element.offsetTop - element.offsetParent.offsetTop);
-        } else {
-          return new Vector2d(element.offsetLeft, element.offsetTop);
-        }
-    }
+  static fromElementOffset(element) {
+    return new Vector2d(element.offsetLeft, element.offsetTop);
+  }
 
-    static fromMatrix(matrix) {
-        return new Vector2d(matrix.translateX, matrix.translateY);
-    }
+  static fromMatrix(matrix) {
+    return new Vector2d(matrix.translateX, matrix.translateY);
+  }
 
-    static fromElementScroll(element) {
-        return new Vector2d(element.scrollLeft, element.scrollTop);
-    }
+  static fromElementScroll(element) {
+    return new Vector2d(element.scrollLeft, element.scrollTop);
+  }
 
-    static fromElementTransform(element) {
-        return Vector2d.fromMatrix(Matrix.fromElementTransform(element));
-    }
+  static fromElementTransform(element) {
+    return Vector2d.fromMatrix(Matrix.fromElementTransform(element));
+  }
 
-    positionElement(element) {
-        element.style.left = `${this.x}px`;
-        element.style.top = `${this.y}px`;
-    }
+  positionElement(element) {
+    element.style.left = `${this.x}px`;
+    element.style.top = `${this.y}px`;
+  }
 
-    positionElementByTranslation(element) {
-        element.style.transform = `translate(${this.x}px, ${this.y}px)`;
-    }
+  positionElementByTranslation(element) {
+    element.style.transform = `translate(${this.x}px, ${this.y}px)`;
+  }
 
-    toString() {
-        return `X: ${this.x}, Y: ${this.y}`;
-    }
+  toString() {
+    return `X: ${this.x}, Y: ${this.y}`;
+  }
 }
 
 module.exports = Vector2d;
