4c3d8cbfcb9c2e4725544ffb7401967f52ed2f4e
Angus MacDonald <angusamacdonald@hotmail.com>
2018-01-31 21:01:59 -0500

    Added setParameterByName fn

--- utils/query-parameters.js
+++ utils/query-parameters.js
@@ -2,16 +2,34 @@ class QueryParameters {
   static getParameterByName(name, urlParam = null) {
     // Cribbed from https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
     const url = urlParam || window.location.href;
-    const parsedName = name.replace(/[\[\]]/g, "\\$&");
-    const regex = new RegExp("[?&]" + parsedName + "(=([^&#]*)|&|#|$)");
+    const parsedName = name.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + parsedName + '(=([^&#]*)|&|#|$)');
     const results = regex.exec(url);
     if (!results) {
       return null;
     } else if (!results[2]) {
       return '';
     } else {
-      return decodeURIComponent(results[2].replace(/\+/g, " "));
+      return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
+  }
+
+  static setParameterByName(name, value, urlParam = null) {
+    // Cribbed from https://stackoverflow.com/questions/5999118/how-can-i-add-or-update-a-query-string-parameter
+    const url = urlParam || window.location.href;
+    const re = new RegExp('([?&])' + name + '=.*?(&|$)', 'i');
+    const separator = url.indexOf('?') !== -1 ? '&' : '?';
+    let endResult;
+    if (url.match(re)) {
+      endResult = url.replace(re, '$1' + name + '=' + value + '$2');
+    }
+    else {
+      endResult = url + separator + name + '=' + value;
+    }
+    if (!urlParam) {
+      window.location.href = endResult;
+    }
+    return endResult;
   }
 }
 
